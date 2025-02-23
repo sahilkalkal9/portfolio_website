5538d3a4bce394671795bbaeee376cc5c692618c
Sahil Kalkal <sahilkalkal108@gmail.com>
2025-02-23 15:58:01 +0530

    added keywords

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export const metadata = {
     title: "Sahil Kalkal",
-    description: "A web developer making websites since 2020. Expertise in HTML, CSS, JavaScript, React JS, Next JS and Google Firebase.",
+    description: "A web developer making websites since 2020. Expertise in HTML, CSS, JavaScript, React JS, Next JS, and Google Firebase.",
 };
 
 export default function Document() {
@@ -12,8 +12,16 @@ export default function Document() {
                 {/* Meta tags */}
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="Hey, I am Sahil Kalkal, a web developer making websites since 2020. Expertise in HTML, CSS, JavaScript, React JS, Next JS and Google Firebase." />
-                <link rel="icon" href="/favicon.ico" />
+                <meta 
+                    name="description" 
+                    content="Hey, I am Sahil Kalkal, a web developer making websites since 2020. Expertise in HTML, CSS, JavaScript, React JS, Next JS, and Google Firebase." 
+                />
+                <meta 
+                    name="keywords" 
+                    content="web developer, frontend developer, React JS, Next JS, JavaScript, HTML, CSS, Firebase, UI/UX, website development, SEO, responsive design" 
+                />
+                <meta name="author" content="Sahil Kalkal" />
+                <link rel="icon" href="/himg.jpg" />
             </Head>
             <body>
                 <Main />
